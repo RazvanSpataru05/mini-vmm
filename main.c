9548ed7161977f10716dbe2ea75a01820b5a24d2
Razvan Spataru <razvanspataru38@gmail.com>
2026-08-08 22:43:24 +0300

    Added error handling for KVM API version and allocated memory for the VM by using mmap sys call

--- main.c
+++ main.c
@@ -5,19 +5,46 @@
 #include <fcntl.h>
 #include <linux/kvm.h>
 
-#define RAM_SIZE 0x10000
+#define RAM_SIZE 0x10000 //1MB of memory for VM
+#define KVM_API_VERSION 12
 
 int main()
 {
     int kvm_fd = open("/dev/kvm", O_RDWR);
+    int version;
+    int vm_fd;
     if (kvm_fd < 0)
     {
         perror("open /dev/kvm");
         return 1;
     }
-    int version = ioctl(kvm_fd, KVM_GET_API_VERSION, 0);
-    printf("KVM Version: %d\n", version);
+
+    version = ioctl(kvm_fd, KVM_GET_API_VERSION, 0);
+    if (version != KVM_API_VERSION)
+    {
+        perror("KVM_GET_API_VERSION");
+        return 1;
+    }
     
-    int vm_fd = ioctl(kvm_fd, KVM_CREATE_VM, 0);
+    vm_fd = ioctl(kvm_fd, KVM_CREATE_VM, 0);
+    if (vm_fd < 0)
+    {
+        perror("KVM_CREATE_VM");
+        return 1;
+    }
+    void* virtual_machine_memory = mmap(NULL, RAM_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+
+    if (virtual_machine_memory == MAP_FAILED)
+    {
+        perror("Error allocating memory for VM");
+        return 1;
+    }
+
+    if (munmap(virtual_machine_memory, RAM_SIZE) != 0)
+    {
+        perror("Error deallocating mmeory for VM");
+        return 1;
+    }
+    printf("VM memory deallocated successfully\n");
     return 0;
 }
