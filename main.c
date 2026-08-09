a4c6123f56ba7e7745fb9df3a0169f383180cb28
Razvan Spataru <razvanspataru38@gmail.com>
2026-08-09 16:08:37 +0300

    Added guest file and loaded its binary into the VM RAM memory

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include <sys/mman.h>
-#include <sys/ioctl.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+#include <sys/mman.h>
+#include <sys/ioctl.h>
 #include <linux/kvm.h>
 
 #define RAM_SIZE 0x10000 //1MB of memory for VM
+#define PAGE_SIZE 4096 // 4096KB/ memory page
 #define KVM_API_VERSION 12
 
 int main()
@@ -32,7 +35,9 @@ int main()
         perror("KVM_CREATE_VM");
         return 1;
     }
-    void* virtual_machine_memory = mmap(NULL, RAM_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    
+    void* virtual_machine_memory = mmap(NULL, RAM_SIZE, PROT_READ | PROT_WRITE,
+         MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0);
 
     if (virtual_machine_memory == MAP_FAILED)
     {
@@ -40,11 +45,43 @@ int main()
         return 1;
     }
 
+    struct kvm_userspace_memory_region mem = {
+        .slot = 0,
+        .guest_phys_addr = 0,
+        .memory_size = RAM_SIZE,
+        .userspace_addr = (uintptr_t)virtual_machine_memory
+    };
+    ioctl(vm_fd, KVM_SET_USER_MEMORY_REGION, &virtual_machine_memory);
+
+    int vcpu_fd = ioctl(vm_fd, KVM_CREATE_VCPU, 0);
+    if (vcpu_fd < 0)
+    {
+        perror("KVM_CREATE_VCPU");
+        return 1;
+    }
+
+    int guest_fd = open("guest/guest", O_RDWR);
+    if (guest_fd < 0)
+    {
+        perror("guest FD");
+        return 1;
+    }
+    char* p = (char*) virtual_machine_memory;
+    for(;;)
+    {
+        int r = read(guest_fd, p, PAGE_SIZE);
+        if (r <= 0) break;
+
+        p += r;
+    }
+    close(guest_fd);
+
     if (munmap(virtual_machine_memory, RAM_SIZE) != 0)
     {
         perror("Error deallocating mmeory for VM");
         return 1;
     }
+
     printf("VM memory deallocated successfully\n");
     return 0;
 }
